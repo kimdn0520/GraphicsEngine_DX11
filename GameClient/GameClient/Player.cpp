fb06688d67fbdb815a15813368b9e38cedb35fc2
JaeWoo <kimdn0520@naver.com>
2023-01-02 23:47:29 +0900

    kimjaewoo

--- GameClient/GameClient/Player.cpp
+++ GameClient/GameClient/Player.cpp
@@ -24,18 +24,10 @@ void Player::Update()
 {
 	if (InputManager::Get()->GetButtonDown(KEY_TYPE::KEY_1))
 	{
-		GetGameObject()->PlayAnim(GetGameObject(), "Armature|Armature.001|mixamo.com|Layer0", true);
+		GetGameObject()->PlayAnim(GetGameObject(), "Armature|Idle", true);
 	}
 	else if (InputManager::Get()->GetButton(KEY_TYPE::KEY_2))
 	{
-		GetGameObject()->PlayAnim(GetGameObject(), "Armature|Armature|mixamo.com|Layer0", true);
-	}
-	else if (InputManager::Get()->GetButton(KEY_TYPE::KEY_3))
-	{
-		GetGameObject()->PlayAnim(GetGameObject(), "Armature|Armature|mixamo.com|Layer0", true);
-	}
-	else if (InputManager::Get()->GetButton(KEY_TYPE::KEY_4))
-	{
-		GetGameObject()->PlayAnim(GetGameObject(), "Armature|Armature|mixamo.com|Layer0", true);
+		GetGameObject()->PlayAnim(GetGameObject(), "Armature|Run", true);
 	}
 }
